cee591ce7086184d6ea8e1e21d285462fb888d36
wannabemountainking <87057243+wannabemountainking@users.noreply.github.com>
2025-11-02 12:45:15 +0900

    NavigationView
    NavigationLink

---
+++ SwiftUIBasics/NavigationViewBasic/NaviBasic2.swift
@@ -0,0 +1,18 @@
+//
+//  NaviBasic2.swift
+//  SwiftUIBasics
+//
+//  Created by YoonieMac on 11/2/25.
+//
+
+import SwiftUI
+
+struct NaviBasic2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NaviBasic2()
+}
---
+++ SwiftUIBasics/NavigationViewBasic/NavigationBasic.swift
@@ -0,0 +1,23 @@
+//
+//  NavigationBasic.swift
+//  SwiftUIBasics
+//
+//  Created by YoonieMac on 11/2/25.
+//
+
+import SwiftUI
+
+struct NavigationBasic: View {
+    // property
+    @State private var showSheet: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            
+        }
+    }
+}
+
+#Preview {
+    NavigationBasic()
+}
